fb62d9695568c6f8d73fcad050b2cffd606cbd7c
a22martinbb <a22martinbb@iessanclemente.net>
2023-01-23 18:00:26 +0100

    s

--- pokemonProyect/pokemon.js
+++ pokemonProyect/pokemon.js
@@ -15,7 +15,6 @@ async function abilityPokemon(url) {
 }
 
 function formatSearch(string) {
-  let invalidCharacters = /_|.| /;
   salida = string.replace(" ", "-");
   salida = salida.toLowerCase();
   return salida;
@@ -30,61 +29,57 @@ input.addEventListener("keypress", (e) => {
 
     switch (searchType) {
       case "pokemon":
-        if (document.getElementById("card") != null) {
-          document.getElementById("card").remove();
+        if (document.getElementById("contenedor") != null) {
+          document.getElementById("contenedor").remove();
         }
 
         let cardPokemon = document.createElement("div");
-        cardPokemon.innerHTML = `<div class="card shadow-sm" id="card">
-                <div class="card-body">
-                  <div class="row">
-                    <div class="col-4 p-3" id="pkmPrincipal">
-                      <div>
+        cardPokemon.innerHTML = `
+                <div class="card shadow-sm" id="contenedor">
+                  <div class="card-body">
+                    <div class="row">
+                      <div class="col-6 col-sm-4 colp-3" id="pkmPrincipal">
+                        <div>
                           <h4 class="text-center" id="pkmName"></h4>
                           <img class="w-100 h-100 mb-3" id="pkmSprite" hidden>
                           <div class="d-flex justify-content-around mb-5" id="pkmTypes"></div>
-                          <table class="text-center">
-                            <thead>
-                              <tr>
-                                <th>HP</th>
-                                <th>ATK</th>
-                                <th>DEF</th>
-                                <th>SPA</th>
-                                <th>SPD</th>
-                                <th>SPE</th>
-                              </tr>
-                            </thead>
-                            <tbody>
-                              <tr>
-                                <td class="pkmStats"></td>
-                                <td class="pkmStats"></td>
-                                <td class="pkmStats"></td>
-                                <td class="pkmStats"></td>
-                                <td class="pkmStats"></td>
-                                <td class="pkmStats"></td>
-                              </tr>
-                            </tbody>
-                            
-                          </table>
-                          
+                        </div>
+                      </div>
           
+                      <div class="col-6 col-sm-8 p-3" id="pkmDescription">
+                        <div id="abilities">
+                            <h5>Abilities:</h5>
+                        </div>
                       </div>
                     </div>
-          
-                    <div class="col-8 p-3" id="pkmDescription">
-                      <div id="abilities">
-                          <h5>Abilities:</h5>
-                          
+                    <div class="row">
+                      <div class="col">
+                        <table class="table text-center w-100">
+                          <thead>
+                            <tr>
+                              <th>HP</th>
+                              <th>ATK</th>
+                              <th>DEF</th>
+                              <th>SPA</th>
+                              <th>SPD</th>
+                              <th>SPE</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <td class="pkmStats"></td>
+                              <td class="pkmStats"></td>
+                              <td class="pkmStats"></td>
+                              <td class="pkmStats"></td>
+                              <td class="pkmStats"></td>
+                              <td class="pkmStats"></td>
+                            </tr>
+                          </tbody>
+                        </table>
                       </div>
                     </div>
-                  
-          
-                
-          
-                </div>
-                </div>
-                  
-                    </div>`;
+                  </div>
+                </div>`;
 
         document.getElementById("container").append(cardPokemon);
 
@@ -106,9 +101,9 @@ input.addEventListener("keypress", (e) => {
             pkmSprite.hidden = false;
 
             //Numero y pokemon
-            document.getElementById("pkmName").innerHTML = `${
+            document.getElementById("pkmName").innerHTML = `#${
               pokemon["id"]
-            }# ${formatter(pokemon["name"])}`;
+            } ${formatter(pokemon["name"])}`;
 
             //TYPES
             pokemon["types"].forEach((pkmType) => {
@@ -158,28 +153,42 @@ input.addEventListener("keypress", (e) => {
           });
         break;
 
+      
+      
+      
+      
       case "ability":
-        if (document.getElementById("card") != null) {
-          document.getElementById("card").remove();
+        if (document.getElementById("contenedor") != null) {
+          document.getElementById("contenedor").remove();
         }
 
         let cardAbility = document.createElement("div");
         cardAbility.innerHTML = `
-                    <div class="card shadow-sm mb-3" id="card">
+                  <div id="contenedor">
+                  <div class="row">
+                    <div class="col-6">
+                      <div class="card shadow-sm mb-3">
                         <div class="card-body">
                             <h4 id="abilityName"></h4>
                             <p id="abilityDescription"></p>
                         </div>
+                      </div>
                     </div>
-                    <div class="card">
-                        <div class="card-body">
-                            <table class="table p-3">
-                                <tbody>
-                            
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>`;
+                    <div class="col-6">
+                      <div class="card">
+                          <div class="card-body">
+                              <table class="table">
+                                  <tbody>
+                              
+                                  </tbody>
+                              </table>
+                          </div>
+                      </div>
+                    </div>
+                  </div>
+                    
+                    
+                  </div>`;
 
         document.getElementById("container").append(cardAbility);
 
@@ -195,22 +204,43 @@ input.addEventListener("keypress", (e) => {
               ability.name
             );
 
-            document.getElementById("abilityDescription").innerHTML =
-              ability.effect_entries[1].effect;
+            console.log(ability.effect_entries[0].language.name);
+            if (ability.effect_entries[0].language.name == "en") {
+              document.getElementById("abilityDescription").innerHTML =
+                ability.effect_entries[0].effect;
+            } else {
+              document.getElementById("abilityDescription").innerHTML =
+                ability.effect_entries[1].effect;
+            }
 
+
+            //LISTA DE POKEMONS
             ability.pokemon.forEach((element) => {
               let tr = document.createElement("tr");
               let tdName = document.createElement("td");
               let tdImg = document.createElement("td");
+              let tdTypes = document.createElement("td");
 
-              proba = abilityPokemon(element.pokemon.url).then(data);
-                console.log(proba);
+              proba = abilityPokemon(element.pokemon.url);
+              proba.then((data) => {
+                tdImg.innerHTML = `<img src=${data["sprites"]["versions"]["generation-viii"]["icons"]["front_default"]} class="w-50 h-50"></img>`;
+
+                data.types.forEach(type => {
+                  console.log(type);
+                  let imgType = document.createElement("img");
+
+                  imgType.src = `img/types/${type.type.name}.png`;
+                  tdTypes.append(imgType);
+                  tdTypes.classList.add("w-10")
+                });
+              });
 
               tdName.innerHTML = formatter(element.pokemon.name);
-              tdImg.innerHTML = `<img src=${data["sprites"]["versions"]["generation-v"]["black-white"][
-                "animated"
-              ]["front_default"]}></img>`;
 
+              tr.classList.add("align-middle");
+              
+              tr.append(tdImg);
+              tr.append(tdTypes);
               tr.append(tdName);
 
               document.querySelector("tbody").append(tr);
@@ -218,6 +248,8 @@ input.addEventListener("keypress", (e) => {
           });
 
         break;
+      
+      
       case "move":
         break;
     }
